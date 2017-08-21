d723b8945d4f217e4d228553102431a1c3507177
vasilvalkov <vasil.valkov@gmail.com>
2017-08-21 23:50:23 +0300

    Added routes for Home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full'}
 ];
 
 @NgModule({
