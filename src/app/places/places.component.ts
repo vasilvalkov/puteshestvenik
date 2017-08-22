554d91a9c4c758324697d9787f342a24086e30f4
vasilvalkov <vasil.valkov@gmail.com>
2017-08-23 01:04:17 +0300

    Added place thumbnails to places page with some fake data for debeloping

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-places',
-  templateUrl: './places.component.html',
-  styleUrls: ['./places.component.css']
+    selector: 'app-places',
+    templateUrl: './places.component.html',
+    styleUrls: ['./places.component.css']
 })
 export class PlacesComponent implements OnInit {
 
-  constructor() { }
+    places: any[] = [
+        {
+            heading: 'Рилски манастир',
+            imageUrl: 'http://www.dinita-tours.com/files/148397219523128.jpg'
+        },
+        {
+            heading: 'Царевец',
+            imageUrl: 'https://i.ytimg.com/vi/Ctb530SGn5w/maxresdefault.jpg'
+        },
+        {
+            heading: 'Хайдушки водопади',
+            imageUrl: 'https://chateauslatina.files.wordpress.com/2014/07/img_15f17.jpg'
+        }
+    ];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
