50afa04e0995e7dd96d1731a0db9df9fd9e03161
vasilvalkov <vasil.valkov@gmail.com>
2017-08-24 01:23:41 +0300

    Added parameterized route for PlaceDetailsComponent

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { PlacesComponent } from './places.component';
+import { PlaceDetailsComponent } from './place-details/place-details.component';
 
 @NgModule({
     imports: [
         RouterModule.forChild([
-            { path: 'places', component: PlacesComponent }
+            { path: 'places', component: PlacesComponent },
+            { path: 'places/:id', component: PlaceDetailsComponent }
         ])
     ],
     exports: [RouterModule]
