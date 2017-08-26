73ca61f9b77289d81a90a75d669a370f79568f59
vasilvalkov <vasil.valkov@gmail.com>
2017-08-26 22:21:35 +0300

    Added Firebase / angularfire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PlacesModule } from './places/places.module';
@@ -15,6 +19,9 @@ import { PlacesModule } from './places/places.module';
 ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
     PlacesModule,
     AppRoutingModule
   ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC9E3X1B6jDXwAPchCt9mgWXlJqdob8iSk',
+    authDomain: 'pateshestvenik-fab85.firebaseapp.com',
+    databaseURL: 'https://pateshestvenik-fab85.firebaseio.com/',
+    projectId: 'pateshestvenik-fab85',
+    storageBucket: 'gs://pateshestvenik-fab85.appspot.com',
+    messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
