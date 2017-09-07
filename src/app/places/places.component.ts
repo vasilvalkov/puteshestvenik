8224f20037fbe341876834aab8ddb5dbb6fa01d7
vasilvalkov <vasil.valkov@gmail.com>
2017-09-07 23:31:12 +0300

    Implemented filtering places by category in PlacesComponent

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -1,3 +1,5 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Place, Category } from './place.model';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { PlaceService } from './shared/place.service';
 import { Component, OnInit } from '@angular/core';
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './places.component.html',
     styleUrls: ['./places.component.css']
 })
-export class PlacesComponent {
-    places = this.placeService.getPlaces();
+export class PlacesComponent implements OnInit {
+    places: FirebaseListObservable<Place[]>; // = this.placeService.getPlaces();
+    categories: FirebaseListObservable<Category[]>;
+    filterForm: FormGroup;
+    allPlaces: Place[];
+    visiblePlaces: Place[] = [];
+    filterBy = '';
 
-    constructor(private placeService: PlaceService) {
+    constructor(private placeService: PlaceService,
+        private builder: FormBuilder) {
+    }
+
+    ngOnInit(): void {
+        this.places = this.placeService.getPlaces();
+        this.categories = this.placeService.getCategories();
+
+        this.filterForm = this.builder.group({
+            categoryName: 'Категория'
+        });
+
+        this.places.subscribe(places => {
+            this.allPlaces = places;
+            this.visiblePlaces = this.filterPlaces(this.filterBy);
+        });
+    }
+
+    filterPlaces(filter) {
+        console.log('filter:', filter);
+        if (filter === '') {
+            return this.allPlaces;
+        }
+
+        return this.allPlaces.filter(p => p.category === filter);
+    }
+
+    getSelectedCategory(event) {
+        this.filterBy = event.target.value;
+        this.visiblePlaces = this.filterPlaces(this.filterBy);
     }
 }
