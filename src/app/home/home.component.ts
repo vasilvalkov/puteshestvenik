63f238441c97380bda45e5b8f5b6442dfd3543f7
vasilvalkov <vasil.valkov@gmail.com>
2017-09-10 20:13:55 +0300

    Implemented Home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,36 @@
+import { Place } from './../places/place.model';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { PlaceService } from './../places/shared/place.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    places: Place[];
+    topPlaces: Place[];
 
-  ngOnInit() {
-  }
+    constructor(private placeService: PlaceService) { }
 
+    ngOnInit() {
+        this.placeService.getPlaces().subscribe(places => {
+            this.places = places;
+            this.topPlaces = this.places
+                .slice()
+                .sort((x, y) => this.compareFunction(x.rating, y.rating))
+                .slice(0, 3);
+        });
+    }
+
+    private compareFunction(a, b) {
+        if (a < b) {
+            return 1;
+        }
+        if (a > b) {
+            return -1;
+        }
+        return 0;
+    }
 }
